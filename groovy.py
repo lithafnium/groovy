236dc570409322e99d980ee9783acd5391f70b09
Steve Li <lithafnium2000@gmail.com>
2021-12-28 12:41:03 -0500

    ffmpeg options

--- groovy.py
+++ groovy.py
@@ -30,8 +30,10 @@ ytdl_format_options = {
     "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
-ffmpeg_options = {"options": "-vn"}
-
+ffmpeg_options = {
+    "options": "-vn",
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
+}
 logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)-15s %(levelname)-8s %(message)s",
@@ -230,7 +232,7 @@ class Music(commands.Cog):
 
 
 bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or("%"),
+    command_prefix=commands.when_mentioned_or("!"),
     description="Relatively simple music bot example",
 )
 
